82e9ccb79dc478b7571a4bc843f3498250af7c4c
Luana Lima <luanatayna2007@gmail.com>
2024-11-26 16:35:39 -0300

    Mais css e novas pastas

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('meus-materiais', views.meus_materiais, name="meus_materiais"),
+    path('area_usuario', views.area_usuario, name="area_usuario"),
+    path('sobre_nos', views.sobre_nos, name="sobre_nos"),
+    path('configuracoes', views.configuracoes, name="configuracoes"),
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,12 @@ def index(request):
 
 def meus_materiais(request):
     return render(request, "meus_materiais.html")
+
+def area_usuario(request):
+    return render(request, "area_usuario.html")
+
+def sobre_nos(request):
+    return render(request, "sobre_nos.html")
+
+def configuracoes(request):
+    return render(request, "configuracoes.html")
